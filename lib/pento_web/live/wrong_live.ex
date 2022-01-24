109f175cf07a6d284fcfff33da33db9c6c72b8ca
sumanp <sumanpuri55@gmail.com>
2022-01-24 22:04:27 +0530

    Programming liveview backup

---
+++ lib/pento_web/live/wrong_live.ex
@@ -0,0 +1,50 @@
+defmodule PentoWeb.WrongLive do
+  use PentoWeb, :live_view
+
+  def mount(_params, session, socket) do
+    { :ok, assign(
+        socket,
+        score: 0,
+        user: Pento.Accounts.get_user_by_session_token(session["user_token"]),
+        message: "Guess a number.",
+        session_id: session["live_socket_id"]
+      )
+    }
+  end
+
+  def render(assigns) do
+    ~L"""
+    <h1>Your score: <%= @score %></h1>
+    <h2>
+      <%= @message %>
+      It's <%= time() %>
+    </h2>
+    <h2>
+      <%= for n <- 1..10 do %>
+        <a href="#" phx-click="guess" phx-value-number="<%= n %>"><%= n %></a>
+      <% end %>
+    </h2>
+    <pre>
+      <%= @user.email %>
+      <%= @session_id %>
+    </pre>
+    """
+  end
+
+  def time() do
+    DateTime.utc_now |> to_string
+  end
+
+  def handle_event("guess", %{"number" => guess}=data, socket) do
+    IO.inspect data
+    message = "Your guess: #{guess}. Wrong. Guess again. "
+    score = socket.assigns.score - 1
+    {
+      :noreply,
+      assign(
+      socket,
+      message: message,
+      score: score)
+    }
+  end
+end
